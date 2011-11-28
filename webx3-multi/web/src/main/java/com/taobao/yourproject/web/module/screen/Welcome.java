a55978c34978e3514f93afe8d578b311ffbf1d8e
yehy225 <yehy225@gmail.com>
2011-11-28 04:20:13 +0000

    webx3-multi工程原型，根据此创建原型。

---
+++ webx3-multi/web/src/main/java/com/taobao/yourproject/web/module/screen/Welcome.java
@@ -0,0 +1,24 @@
+package com.taobao.yourproject.web.module.screen;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+import com.alibaba.citrus.turbine.Context;
+
+/**
+ * Welcome Screen
+ * @author yingyang
+ *
+ */
+public class Welcome {
+	
+	@Resource HttpServletRequest request;
+    /**
+     * display welcome screen
+     * @param context
+     */
+	public void execute(Context context)  {
+		
+        context.put("name", "yingyang");
+    }
+}
